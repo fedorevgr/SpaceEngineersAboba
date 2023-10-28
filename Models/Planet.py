88b178d688ef509925a6ae53be02528614f0ffe1
mkholkin <mkholkin@h88.net55.bmstu.ru>
2023-10-28 17:41:32 +0300

    create planet model
    q
    z

---
+++ Models/Planet.py
@@ -0,0 +1,16 @@
+from scipy.constants import G
+
+
+class Planet:
+    radius: float
+    mass: float
+    gravitational_parameter: float
+    x: int = 0
+    y: int = 0
+
+    def __init__(self, radius: float, mass: float) -> None:
+        self.radius = radius
+        self.mass = mass
+        self.gravitational_parameter = radius*mass
+
+        
